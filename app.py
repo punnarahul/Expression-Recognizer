ca8b7866c3851be7244f20e5f10072b99e38c46d
punna rahul <punnarahul2354lp@gmail.com>
2021-08-08 16:34:46 +0530

    Initialize commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model=keras.models.load_model('static/model1')
 app=Flask(__name__)
 
 
-@app.route()
+@app.route('/')
 def shower():
     return render_template('index.html')
 
@@ -25,13 +25,9 @@ def upload_files():
     uploaded_file.save(uploaded_file.filename)
     imgname=uploaded_file.filename
     img=cv2.imread(imgname)
-    print('orginal shape :',img.shape)
     img=cv2.resize(img,(48,48))
-    print('resizing shape(32,32,3) :',img.shape)
     img=np.array(img)
     img=img/255
-    print('/255 operation:',img)
-    print(img.shape)
     # img=img.reshape(-1,6912)
     img = img.reshape(-1, 48, 48, 3)
     value=model.predict([img])
@@ -50,7 +46,7 @@ def upload_files():
     elif i==5:
         return render_template('index.html',img=imgname,disgust='disgust')   
     else:
-        return render_template('index.html',img=imgname,fear='fear')     
+        return render_template('index.html',img=imgname,fear='fear')      
     
 if __name__=='__main__':
     app.run(debug=True)
